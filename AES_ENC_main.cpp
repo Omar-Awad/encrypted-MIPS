d673679f6250f6c21c7ef7b9d84e31e9585fe5c8
xkldx <xkldx20@gmail.com>
2018-03-31 22:29:56 +0200

    Encryption and key expansion done
    Encryption and key expansion  fully completed and tested

--- AES_ENC_main.cpp
+++ AES_ENC_main.cpp
@@ -1,17 +1,17 @@
-/*#include "systemc.h"
-#include <iostream>
-#include "AES_ENC.h"
-
+#include"systemc.h"
+#include"AES_ENC.h"
 int sc_main(int argc, char* argv[]){
-//PLAINTEXT: 00112233445566778899aabbccddeeff
-//KEY: 000102030405060708090a0b0c0d0e0f
-sc_in<sc_lv<8>> key[4][4], plaintext[4][4];
-sc_lv<8> cipher;
-
-sc_lv<8> plain_sig[4][4] = {{0x00, 0x11, 0x22, 0x33}, {0x44, 0x55, 0x66, 0x77},{0x88, 0x99, 0xaa, 0xbb}, {0xcc, 0xdd, 0xee, 0xff}};
-sc_lv<8> key_sig[4][4] = {{0x00, 0x01, 0x02, 0x03},{0x04, 0x05, 0x06, 0x07},{0x08, 0x09, 0x0a, 0x0b},{0x0c, 0x0d, 0x0e, 0x0f}};
-
-
-sc_start();
-return 0;
-}*/
+	sc_clock clk("clk", 20, SC_NS);
+	sc_signal<sc_logic> reset;
+	sc_signal<sc_lv<128>> keyIn;
+	sc_signal<sc_lv<128>> txt;
+	sc_signal<sc_lv<128>> cipher;
+	txt = "0x3243f6a8885a308d313198a2e0370734";
+	keyIn = "0x02b7e151628aed2a6abf7158809cf4f3c";
+	reset = SC_LOGIC_0;
+	AES_ENC AES_ENC1("AES_ENC1");
+	AES_ENC1(clk, reset, txt, keyIn, cipher);
+	sc_start(40, SC_NS);
+	cout << cipher;
+	return 0;
+}
