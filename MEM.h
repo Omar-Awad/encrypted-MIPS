6291085aa22556cd17b4b1d703d8a203c968f740
Omar Awad <omaralaa14895@gmail.com>
2018-04-13 16:33:51 +0200

    Update MEM.h

--- MEM.h
+++ MEM.h
@@ -6,7 +6,7 @@ sc_in<sc_uint<5>> mem_addr; //addr for read/write in mem
 sc_in<sc_lv<128>> mem_wb;
 sc_in<bool> w_r, inst_data; //w_r=1 (write), w_r=0 (read), inst_data=1(inst), inst_data=0(data)
 sc_out<sc_lv<128>> mem_out;
-sc_lv<128> myMem[32]; // initialize it hear
+sc_lv<128> myMem[32]; // initialize it here
 void mem_func(){
 	if(clk.event() && clk)
 		if(inst_data == 1)
